07e88d7d2ab2c89f334b67cac9caf0ec7a1e7c46
sir_gavith <gavinrvsmith@gmail.com>
2022-12-05 21:33:41 -0800

    a

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { Glib } from './Glib/main'
 Glib.init();
-import * as Advent2019 from './Advent2019';
-import { Advent2021 } from './Advent2021';
 import * as Advent2022 from './Advent2022'
-import { Sorts } from './Glib/Sort';
 
 const startTime = process.hrtime()
 
